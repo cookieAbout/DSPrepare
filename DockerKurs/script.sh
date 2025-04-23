463cf60727fca724b65935c214217bd0cc106b1d
se.panchenko <say.sokolowa@gmail.com>
2025-04-23 13:46:11 +0700

    https://lab.karpov.courses/learning/102/module/1276/lesson/12128/35059/177465/ Работа с файлами

--- DockerKurs/script.sh
+++ DockerKurs/script.sh
@@ -8,4 +8,11 @@
 #else
 #  echo "Hello, ${first_letter^}${rest_letters,,} :)"
 #fi
-echo ":)"
+
+#echo ":)"
+
+if [ ! -e names.txt ]; then
+    echo "Не могу найти файл 'names.txt'"
+    exit 1
+fi
+cat names.txt | awk '{print "Hello, "$1}'
